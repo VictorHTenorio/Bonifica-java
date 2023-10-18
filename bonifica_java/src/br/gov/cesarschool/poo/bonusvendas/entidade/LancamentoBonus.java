48143455e677ed22af26685637214795f4757f08
VictorHTenorio <victorhorten7@gmail.com>
2023-10-17 23:47:22 -0300

    lancamentoBonusDao

--- bonifica_java/src/br/gov/cesarschool/poo/bonusvendas/entidade/LancamentoBonus.java
+++ bonifica_java/src/br/gov/cesarschool/poo/bonusvendas/entidade/LancamentoBonus.java
@@ -8,19 +8,19 @@ public class LancamentoBonus implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private long numeroDeCaixasDeBonus;
+	private long numeroCaixaDeBonus;
 	private double valor;
 	private java.time.LocalDateTime dataHoraLancamento;
 	
 	public LancamentoBonus(long numeroDeCaixasDeBonus, double valor, LocalDateTime dataHoraLancamento) {
 		super();
-		this.numeroDeCaixasDeBonus = numeroDeCaixasDeBonus;
+		this.numeroCaixaDeBonus = numeroDeCaixasDeBonus;
 		this.valor = valor;
 		this.dataHoraLancamento = dataHoraLancamento;
 	}
 
-	public long getNumeroDeCaixasDeBonus() {
-		return numeroDeCaixasDeBonus;
+	public long getNumeroCaixaDeBonus() {
+		return numeroCaixaDeBonus;
 	}
 
 	public double getValor() {
