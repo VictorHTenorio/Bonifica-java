5fb40e083e92d90d7f6e15f87bd37632d90e26d3
Cenafowzin <digodubeux@gmail.com>
2023-10-19 02:13:07 -0300

    mudando project

---
+++ bonifica_java/src/br/gov/cesarschool/poo/bonusvendas/gui/TelaManutencaoVendedor.java
@@ -0,0 +1,50 @@
+package br.gov.cesarschool.poo.bonusvendas.gui;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.SWT;
+
+public class TelaManutencaoVendedor {
+
+	protected Shell shell;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			TelaManutencaoVendedor window = new TelaManutencaoVendedor();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+
+	}
+
+}
