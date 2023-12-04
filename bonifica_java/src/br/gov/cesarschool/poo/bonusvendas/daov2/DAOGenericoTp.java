acba34f3a0c281863f822c71d7acfc62ddf3cec1
joaohlafeta <105346791+joaohlafeta@users.noreply.github.com>
2023-12-04 16:06:15 -0300

    Criando o Daogenericotp

---
+++ bonifica_java/src/br/gov/cesarschool/poo/bonusvendas/daov2/DAOGenericoTp.java
@@ -0,0 +1,47 @@
+package br.gov.cesarschool.poo.bonusvendas.daov2;
+
+import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
+import br.gov.cesarschool.poo.bonusvendas.entidade.geral.Registro;
+import br.gov.cesarschool.poo.bonusvendas.excecoes.ExcecaoObjetoJaExistente;
+import br.gov.cesarschool.poo.bonusvendas.excecoes.ExcecaoObjetoNaoExistente;
+
+public class DAOGenericoTp<T extends Registro> {
+    CadastroObjetos cadastro;
+    private String nomeEntidade;
+
+    public DAOGenericoTp(Class<T> tipo, String nomeEntidade) {
+        this.cadastro = new CadastroObjetos(tipo);
+        this.nomeEntidade = nomeEntidade;
+    }
+
+    public void incluir(T reg) throws ExcecaoObjetoJaExistente {
+        try {
+            buscar(reg.getIdUnico());
+            throw new ExcecaoObjetoJaExistente(nomeEntidade + " ja existente");
+        } catch (ExcecaoObjetoNaoExistente e) {
+            cadastro.incluir(reg, reg.getIdUnico());
+        }
+    }
+
+    public void alterar(T reg) throws ExcecaoObjetoNaoExistente {
+        buscar(reg.getIdUnico());
+        cadastro.alterar(reg, reg.getIdUnico());
+    }
+
+    public T buscar(String id) throws ExcecaoObjetoNaoExistente {
+        T registroBusca = (T) cadastro.buscar(id);
+        if (registroBusca == null) {
+            throw new ExcecaoObjetoNaoExistente(nomeEntidade + " nao existente");
+        }
+        return registroBusca;
+    }
+
+    public T[] buscarTodos() {
+        Object[] rets = cadastro.buscarTodos(tipo);
+        T[] registros = (T[]) new Registro[rets.length]; // Cast para T[]
+        for (int i = 0; i < rets.length; i++) {
+            registros[i] = (T) rets[i];
+        }
+        return registros;
+    }
+}
